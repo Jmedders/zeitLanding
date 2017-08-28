39ef6a2d96c6296ab7b24e735db85cd3481caf3f
Jeff <medders.jeffrey@gmail.com>
2017-08-28 09:47:03 -0600

    update call page to white

--- angular/javascripts/controllers/controllers.js
+++ angular/javascripts/controllers/controllers.js
@@ -1,4 +1,13 @@
 app.controller("landingController", ["$scope", "$rootScope", "$routeParams", "$location", "$state", "$stateParams", "$window",
 function($scope, $rootScope, $routeParams, $location, $state, $stateParams, $window){
   $scope.view = {};
+  $scope.$on('$routeChangeStart', function(next, current) {
+    if($location.path() == '/call'){
+      $('html, body').attr("style", "background: white;");
+    }
+    if($location.path() !== '/call'){
+      $('html, body').attr("style", "background: rgb(8, 9, 11);")
+      console.log('true');
+    }
+  });
 }])
